5ee2cca0c4cb2cba5993fcbce2cf964d60afe876
Sprow <ts.sprow@gmail.com>
2019-06-21 23:38:25 +0300

    fix settings

--- chat_room/settings.py
+++ chat_room/settings.py
@@ -65,12 +65,12 @@ WSGI_APPLICATION = 'chat_room.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
